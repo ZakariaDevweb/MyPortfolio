be784d63be6d4dc5bd31fb30e9bf21c148cc17b5
ZakariaDevweb <zakaria.messali25@gmail.com>
2024-09-05 17:03:29 +0200

    suppression du modale pour simplifier la modale lorsqu"on clique sur un titre des  3 projets ça affiche un aperçu

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ async function getProjects() {
         }
         const projects = await response.json();
         projects.forEach(project => {
-            const projectHtml = `<div class="project">
-                <div class="openModal">
+            const projectHtml = `<div class=" project">
+                <a href="${project.urlrender}">
                     <div class="header-project">${project.name}</div>
                     <div class="body-project"><img src="${project.pathimage}"></div>
-                </div>
+                </a>
                 <a href="${project.githubrepo}">
                     <div class="footer-project">👑 Lien Github 👑</div>
                 </a>
@@ -23,48 +23,13 @@ async function getProjects() {
 
         });
 
-        const lstCard = document.getElementsByClassName("openModal");
-        console.log(lstCard)
-        Array.from(lstCard).forEach(item => {
-            item.addEventListener("click", () => {
-                createModalElements();
-            })
-        })
+     
     } catch (error) {
         console.error(`les projets sont impossible a charger: ${error}`);
 
     }
 
 };
-let modalContainer = "";
-let modal = "";
-let modalGallery = "";
-
-function createModalElements() {
-    const body = document.body;
-    modalContainer = document.createElement("div");
-
-    modalContainer.classList.add("modal-container");
-    body.appendChild(modalContainer);
-
-
-    modal = document.createElement("div");
-    modal.classList.add("modal");
-    modalContainer.appendChild(modal);
-
-    modalTitle = document.createElement("h1");
-    modalTitle.classList.add("modal-title");
-    modalTitle.innerHTML = "Galerie photo";
-    modal.appendChild(modalTitle);
-
-    modalGallery = document.createElement("div");
-    modalGallery.classList.add("modal-content", "modal-gallery");
-    modal.appendChild(modalGallery);
-
-
-
-}
-
 
 getProjects();
 
