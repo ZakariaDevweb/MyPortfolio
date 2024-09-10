33a22af3f7a10578fa6d7fc4c911f160bf7635c7
ZakariaDevweb <zakaria.messali25@gmail.com>
2024-09-10 15:55:00 +0200

    ajout de la descriptions et du formulaire pour communicatio,

--- script.js
+++ script.js
@@ -37,4 +37,16 @@ async function getProjects() {
 };
 
 getProjects();
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_42hzjhn', 'template_5agjrio', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
 
